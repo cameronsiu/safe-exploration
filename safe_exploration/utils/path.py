232fe238d83945baff61f1b5967b38b7e277e2e8
Cameron Siu <cameronsiu02@gmail.com>
2025-10-20 11:36:16 -0700

    fixed root dir path

--- safe_exploration/utils/path.py
+++ safe_exploration/utils/path.py
@@ -5,7 +5,7 @@ def get_project_root_dir() -> Path:
     """Return the absolute path to the project root."""
     # Get the file path of the caller
     current_file = Path(inspect.getfile(inspect.currentframe().f_back)).resolve()
-    # Move up two levels: src/safe_exploration -> src -> project root
+    # Move up two levels: safe_exploration -> project root
     return current_file.parents[2]
 
 def get_config_path(filename: str = "defaults.yml") -> Path:
