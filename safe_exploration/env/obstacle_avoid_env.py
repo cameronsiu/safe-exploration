a979aa30d7d7832481f88114dfe74e8dc2b81d02
Cameron Siu <cameronsiu02@gmail.com>
2025-11-10 05:57:49 -0800

    added more

--- safe_exploration/env/obstacle_avoid_env.py
+++ safe_exploration/env/obstacle_avoid_env.py
@@ -21,6 +21,7 @@ import omni
 from collections.abc import Sequence
 
 import isaaclab.sim as sim_utils
+import isaaclab.utils.math as math_utils
 from isaaclab.scene import InteractiveScene
 from isaaclab.sim import SimulationContext
 from isaaclab.utils import configclass
@@ -94,6 +95,8 @@ class ObstacleAvoidEnv(DirectRLEnv):
         self.dof_idx, _ = self.robot.find_joints(self.cfg.dof_names)
 
     def _setup_scene(self):
+        ##### TODO: try device=cuda after .cuda()
+
         self.robot = Articulation(self.cfg.robot_cfg)
 
         spawn_ground_plane(prim_path="/World/ground", cfg=GroundPlaneCfg())
@@ -111,10 +114,23 @@ class ObstacleAvoidEnv(DirectRLEnv):
         self.commands = self.commands/torch.linalg.norm(self.commands, dim=1, keepdim=True)
 
         ratio = self.commands[:, 1]/(self.commands[:,0]+1E-8)
+        gzero = torch.where(self.commands > 0, True, False)
+        lzero = torch.where(self.commands < 0, True, False)
+        plus = lzero[:, 0]*gzero[:, 1]
+        minus = lzero[:,0]*lzero[:,1]
+        offsets = torch.pi*plus - torch.pi*minus
+        self.yaws = torch.atan(ratio).reshape(-1,1) + offsets.reshape(-1,1)
+
+        self.marker_locations = torch.zeros((self.cfg.scene.num_envs, 3)).cuda()
+        self.marker_offset = torch.zeros((self.cfg.scene.num_envs, 3)).cuda()
+        self.marker_offset[:,-1] = 0.5
+        self.forward_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4)).cuda()
+        self.command_marker_orientations = torch.zeros((self.cfg.scene.num_envs, 4)).cuda()
 
     def _pre_physics_step(self, actions: torch.Tensor) -> None:
         # actions shape [num_envs, num_actions]
         self.actions = actions.clone()
+        self._visualize_markers()
 
     def _apply_action(self) -> None:
         self.robot.set_joint_velocity_target(self.actions, joint_ids=self.dof_idx)
@@ -123,12 +139,16 @@ class ObstacleAvoidEnv(DirectRLEnv):
         # self.robot is an Articulation
         # self.robot.data is an ArticulationData
         # ArticulationData contains all cloned robots
-        self.velocity = self.robot.data.root_com_lin_vel_b
+        self.velocity = self.robot.data.root_com_vel_w
+        self.forwards = math_utils.quat_apply(self.robot.data.root_link_quat_w, self.robot.data.FORWARD_VEC_B)
+        obs = torch.hstack((self.velocity, self.commands))
         observations = {"policy": self.velocity}
         return observations
     
     def _get_rewards(self) -> torch.Tensor:
-        total_reward = torch.linalg.norm(self.velocity, dim=-1, keepdim=True)
+        forward_reward = self.robot.data.root_com_lin_vel_b[:,0].reshape(-1,1)
+        alignment_reward = torch.sum(self.forwards * self.commands, dim=-1, keepdim=True)
+        total_reward = forward_reward + alignment_reward
         return total_reward
     
     def _get_dones(self) -> tuple[torch.Tensor, torch.Tensor]:
@@ -141,10 +161,43 @@ class ObstacleAvoidEnv(DirectRLEnv):
             env_ids = self.robot._ALL_INDICES
         super()._reset_idx(env_ids)
 
+        # pick new commands for reset envs
+        self.commands[env_ids] = torch.randn((len(env_ids), 3)).cuda()
+        self.commands[env_ids,-1] = 0.0
+        self.commands[env_ids] = self.commands[env_ids]/torch.linalg.norm(self.commands[env_ids], dim=1, keepdim=True)
+
+        # recalculate the orientations for the command markers with the new commands
+        ratio = self.commands[env_ids][:,1]/(self.commands[env_ids][:,0]+1E-8)
+        gzero = torch.where(self.commands[env_ids] > 0, True, False)
+        lzero = torch.where(self.commands[env_ids]< 0, True, False)
+        plus = lzero[:,0]*gzero[:,1]
+        minus = lzero[:,0]*lzero[:,1]
+        offsets = torch.pi*plus - torch.pi*minus
+        self.yaws[env_ids] = torch.atan(ratio).reshape(-1,1) + offsets.reshape(-1,1)
+    
+        # set the root state for the reset envs
         default_root_state = self.robot.data.default_root_state[env_ids]
         default_root_state[:, :3] += self.scene.env_origins[env_ids]
 
         self.robot.write_root_state_to_sim(default_root_state, env_ids)
+        self._visualize_markers()
+
+    def _visualize_markers(self):
+        # get marker locations and orientations
+        self.marker_locations = self.robot.data.root_pos_w
+        self.forward_marker_orientations = self.robot.data.root_quat_w
+        self.command_marker_orientations = math_utils.quat_from_angle_axis(self.yaws, self.up_dir).squeeze()
+
+        # offset markers so they are above the jetbot
+        loc = self.marker_locations + self.marker_offset
+        loc = torch.vstack((loc, loc))
+        rots = torch.vstack((self.forward_marker_orientations, self.command_marker_orientations))
+
+        # render the markers
+        all_envs = torch.arange(self.cfg.scene.num_envs)
+        indices = torch.hstack((torch.zeros_like(all_envs), torch.ones_like(all_envs)))
+        self.visualization_markers.visualize(loc, rots, marker_indices=indices)
+
 
 def define_markers() -> VisualizationMarkers:
     marker_cfg = VisualizationMarkersCfg(
